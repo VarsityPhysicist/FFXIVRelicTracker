f0e286ff596840540442ee75491f4fffab642863
Justin Lata <latajustin@gmail.com>
2020-10-03 08:53:21 -0400

    Removed internal from property

--- ARR/Animus/AnimusModel.cs
+++ ARR/Animus/AnimusModel.cs
@@ -71,7 +71,7 @@ namespace FFXIVRelicTracker.ARR.Animus
         public bool netherFall1Book{ get; set;}
         public bool skyWind1Book{ get; set;}
         public bool skyEarth1Book{ get; set;}
-        public bool skyWind2Book { get; internal set; }
+        public bool skyWind2Book { get; set; }
         public int bookSelection{ get; set; }
 
         public AnimusObject Leve1{ get; set;}
